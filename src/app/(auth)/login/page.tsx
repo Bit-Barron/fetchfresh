6553fed63b44af6b7654a5d30ab95a9119a0462f
Bit-Barron <azerabdu5@gmail.com>
2024-09-08 00:52:52 +0200

    asd

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,14 +1,5 @@
 "use client";
 
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { FormEvent } from "react";
-import { Card, CardContent } from "@/components/ui/card";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { toast, Toaster } from "sonner";
-
 export default function LoginPage() {
   return <div>Login Page</div>;
 }
