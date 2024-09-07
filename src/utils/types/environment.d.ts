aee16a287eb1ef3bcbe2951bccdd2526a882ecb2
Bit-Barron <azerabdu5@gmail.com>
2024-09-08 01:53:23 +0200

    asd

--- src/utils/types/environment.d.ts
+++ src/utils/types/environment.d.ts
@@ -3,5 +3,9 @@ declare namespace NodeJS {
     SECRET: string;
     NEXT_PUBLIC_URL: string;
     DATABASE_URL: string;
+    NEXT_PUBLIC_API_URL: string;
+    NEXT_PUBLIC_SMTP_HOST: string;
+    NEXT_PUBLIC_SMTP_USER: string;
+    NEXT_PUBLIC_SMTP_PASS: string;
   }
 }
