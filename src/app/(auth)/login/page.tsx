59ed517bcd76471c0badf925b07f920beb6b9e6c
Bit-Barron <azerabdu5@gmail.com>
2024-09-08 02:33:53 +0200

    asd

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -40,7 +40,7 @@ export default function LoginPage(props: LoginPageProps) {
       <div className="w-full max-w-md space-y-8">
         <div>
           <h2 className="text-foreground mt-6 text-center text-3xl font-bold tracking-tight">
-            Einloggens
+            Einloggenssa
           </h2>
         </div>
         <Card className="">
